9f338a285db2ace551e4b5b210bceae73acfe7d9
Devkant <devkant21@gmail.com>
2022-02-04 15:33:24 +0530

    Authentication added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login'
+export class App extends Component {
+  responseGoogle=(response) => {
+    console.log(response);
+    console.log(response.profileObj);
+  }
+  render() {
+    return (
+      <div>
+        <GoogleLogin
+        clientId="361465968393-gi04rvo6lbceuq64gjqsrqk47dgm64e5.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+        cookiePolicy={'single_host_origin'}
+
+        />
+
+
+    </div>
+    )
+  }
+}
+
+export default App;
+
